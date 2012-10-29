c034b550f26d4ea3311eb6823cc1a6d5edf51f33
Mike Sushkov <msushkov@msushkov-desktop.(none)>
2012-10-29 01:26:38 -0700

    Working on SVD implementation in C++. Still getting seg fault...

--- c++/SVDstart.cpp
+++ c++/SVDstart.cpp
@@ -11,57 +11,49 @@
 using namespace std;
 
 // Filenames
-const char dataFilePath[] = "/home/msushkov/Dropbox/Caltech/Caltech Classes/CS/CS 156b/data/mu/data1.txt";
+const char dataFilePath[] = "/home/msushkov/Dropbox/Caltech/Caltech Classes/CS/CS 156b/data/mu/data2.txt";
 const char qualFilePath[] = "/home/msushkov/Dropbox/Caltech/Caltech Classes/CS/CS 156b/data/mu/qual.dta";
 const char outputFilePath[] = "/home/msushkov/Dropbox/Caltech/Caltech Classes/CS/CS 156b/submissions/SVDstart.txt";
 
 // constants
-const int NUM_FEATURES = 40;
+const int NUM_FEATURES = 6;
 const int NUM_USERS = 458293;
 const int NUM_MOVIES = 17770;
 const double LEARNING_RATE = 0.001;
 const int K = 25;
 const int EPSILON = 10^-5;
-const int MAX_ITERATIONS = 150;
+const int MAX_ITERATIONS = 20;
 
 // feature matrices (2D arrays)
 // feature k for user u is userFeatures[u][k]
-//double **userFeatures = new double[NUM_USERS][NUM_FEATURES];
-//double **movieFeatures = new double[NUM_MOVIES][NUM_FEATURES];
-
 double **userFeatures = new double*[NUM_USERS];
 double **movieFeatures = new double*[NUM_MOVIES];
 
 // initialize each element of the feature matrices to 0.1
 void initialize() {
-/*
-    for (int i = 0; i < NUM_USERS; i++) {
-        for (int j = 0; j < NUM_FEATURES; j++)
-            //userFeatures[i * NUM_FEATURES + j] = 0.1; //userFeatures[i][j]
-            userFeatures[i][j] = 0.1;
-    }
-
-    for (int i = 0; i < NUM_MOVIES; i++) {
-        for (int j = 0; j < NUM_FEATURES; j++)
-            //movieFeatures[i * NUM_FEATURES + j] = 0.1;
-            movieFeatures[i][j] = 0.1;
-    }*/
-    
+    // initialize the user feature matrix to all 0.1's
     for (int i = 0; i < NUM_USERS; i++)
         userFeatures[i] = new double[NUM_FEATURES];
+    for (int i = 0; i < NUM_USERS; i++) {
+        for (int j = 0; j < NUM_FEATURES; j++)
+            userFeatures[i][j] = 0.1;
+    }
         
+    // initialize the movie feature matrix to all 0.1's
     for (int i = 0; i < NUM_MOVIES; i++)
         movieFeatures[i] = new double[NUM_FEATURES];
-    
+    for (int i = 0; i < NUM_MOVIES; i++) {
+        for (int j = 0; j < NUM_FEATURES; j++)
+            movieFeatures[i][j] = 0.1;
+    }
 }
 
 // helper function that splits a string
 vector<string> &split(const string &s, char delim, vector<string> &elems) {
     stringstream ss(s);
     string item;
-    while(getline(ss, item, delim)) {
+    while(getline(ss, item, delim))
         elems.push_back(item);
-    }
     return elems;
 }
 
@@ -71,17 +63,17 @@ vector<string> split(const string &s, char delim) {
     return split(s, delim, elems);
 }
 
-// calculate the dot product of 2 arrays
-double dotProduct(double *list1, double *list2) {
-    double result = 0.0;
-    for (int i = 0; i < sizeof(list1) / sizeof(list1[0]); i++)
-        result += list1[i] * list2[i];
-    return result;
-}
-
 // calculates the dot product of the feature vectors for a given user and movie
 double predictRating(int movie, int user) {
-    return dotProduct(userFeatures[user], movieFeatures[movie]);
+    double result = 0.0;
+
+    for (int i = 0; i < NUM_FEATURES; i++) {
+        cout << userFeatures[user][i] << " :: " << movieFeatures[movie][i] << endl;
+        double prod = (userFeatures[user][i]) * (movieFeatures[movie][i]);
+        result += prod;
+    }
+
+    return result;
 }
 
 // trains our algorithm given an input data point
@@ -93,26 +85,33 @@ void trainSVD(int user, int movie, int rating) {
     
     // learn while our error is decreasing by an amount higher than epsilon
     // or until we hit some # of iterations
-    while (abs(LEARNING_RATE * (rating - predictRating(movie - 1, user - 1)) - error) > EPSILON && numIterations < MAX_ITERATIONS) {
+    //while (abs(LEARNING_RATE * (rating - predictRating(movie - 1, user - 1)) - error) > EPSILON && numIterations < MAX_ITERATIONS) {
+    while (numIterations < MAX_ITERATIONS) {
         // calculate the new error
-        error = LEARNING_RATE * (rating - predictRating(movie - 1, user - 1));
+        //cout << predictRating(movie - 1, user - 1) << endl;
+
+        error = rating - predictRating(movie - 1, user - 1);
+        //cout << "ERROR: " << error << endl;
+        //cout << user << "::" << movie << endl;
     
         // iterate over features 0 through 39
-        for (int k = 0; k < NUM_FEATURES; k++) {
-            /*double oldUserFeature = userFeatures[user * NUM_FEATURES + k];
-            userFeatures[user * NUM_FEATURES + k] += LEARNING_RATE * (error * movieFeatures[movie * NUM_FEATURES + k] - K * oldUserFeature);
-            movieFeatures[movie * NUM_FEATURES + k] += LEARNING_RATE * (error * oldUserFeature - K * movieFeatures[movie * NUM_FEATURES + k]);*/
-            
+        for (int k = 0; k < NUM_FEATURES; k++) {            
             double oldUserFeature = userFeatures[user - 1][k];
-            userFeatures[user - 1][k] += LEARNING_RATE * (error * movieFeatures[movie - 1][k] - K * oldUserFeature);
-            movieFeatures[movie - 1][k] += LEARNING_RATE * (error * oldUserFeature - K * movieFeatures[movie - 1][k]);
+            //userFeatures[user - 1][k] += LEARNING_RATE * (error * movieFeatures[movie - 1][k] - K * oldUserFeature);
+            //movieFeatures[movie - 1][k] += LEARNING_RATE * (error * oldUserFeature - K * movieFeatures[movie - 1][k]);
+
+            userFeatures[user - 1][k] += LEARNING_RATE * error * movieFeatures[movie - 1][k];
+            movieFeatures[movie - 1][k] += LEARNING_RATE * error * oldUserFeature;
+
+            cout << userFeatures[user - 1][k] << endl;
+            cout << movieFeatures[movie - 1][k] << endl;
         }
             
         numIterations++;
     }
 }
 
-// reads data in
+// reads data in and loads it into memory
 void getData() {
     string line;
     ifstream dataFile(dataFilePath, ios::in);
@@ -121,21 +120,18 @@ void getData() {
     
     // go through the input data line by line
     if (dataFile.is_open()) {
-        while (dataFile.good()) {
+        while (dataFile.good() && numLinesSoFar < 10) {
             if (getline(dataFile, line)) {                
                 vector<string> vals = split(line, ' ');
-                
+
                 // call the training function using the given data point
                 trainSVD(atoi(vals[0].c_str()), atoi(vals[1].c_str()), atoi(vals[3].c_str()));
                 
                 numLinesSoFar++;
                 if (numLinesSoFar % 100000 == 0)
                     cout << numLinesSoFar << endl;
-                
             }
         }
-        
-        cout << "Here" << endl;
     }
     
     dataFile.close();
@@ -155,17 +151,18 @@ void outputResults() {
         while (qualFile.good()) {
             if (getline(qualFile, line)) {
                 vector<string> vals = split(line, ' ');
-                
-                string userStr = vals[0];
-                string movieStr = vals[1];
-                
-                // convert both to integers
-                int user = atoi(userStr.c_str());
-                int movie = atoi(movieStr.c_str());
-                
-                // calculate the rating
-                double predictedScore = predictRating(user - 1, movie - 1);
-                
+
+                //cout << atoi(vals[0].c_str()) - 1 << " :: " << atoi(vals[1].c_str()) - 1<< endl;
+
+                int user = atoi(vals[0].c_str()) - 1;
+                cout << user << endl;
+                /*for (int j = 0; j < NUM_FEATURES; j++) {
+                    cout << userFeatures[1][j] << ", ";
+                }*/
+
+                // calculate the rating for user, movie
+                double predictedScore = predictRating(atoi(vals[0].c_str()) - 1, atoi(vals[1].c_str()) - 1);
+
                 // write to file
                 if (outputFile.is_open()) {
                     // convert double to string first
@@ -183,14 +180,7 @@ void outputResults() {
     }
 }
 
-int main() {
-    initialize();
-    getData();
-    
-    cout << "Done with getData()" << endl;
-    
-    outputResults();
-    
+void cleanup() {
     for (int i = 0; i < NUM_USERS; i++)
         delete[] userFeatures[i];
         
@@ -199,9 +189,37 @@ int main() {
         
     delete[] userFeatures;
     delete[] movieFeatures;
+}
+
+void debugPrint() {
+    for (int i = 0; i < NUM_USERS; i++) {
+        for (int j = 0; j < NUM_FEATURES; j++) {
+            cout << userFeatures[i][j] << ", ";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < NUM_MOVIES; i++) {
+        for (int j = 0; j < NUM_FEATURES; j++) {
+            cout << movieFeatures[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    initialize();
+
+    getData();
+    cout << "Done with getData()" << endl;
     
+    outputResults();
+    //debugPrint();
+    cout << "Done with outputResults()" << endl;
+
+    cleanup();
+
     cout << "DONE!" << endl;
     
     return 0;
-}   
-   
+}
